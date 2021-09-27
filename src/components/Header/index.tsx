b4ec58d277c27322e368bfff5d4d978d5f35e0de
João Vitor <infamoz_br3@hotmail.com>
2021-09-27 18:46:19 -0300

    :shipit: cria componente para link entre as pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { ActiveLink } from "../ActiveLink";
+
 import { SigninButton } from "../SigninButton";
 import styles from "./styles.module.scss";
 
@@ -7,8 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/" prefetch>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SigninButton />

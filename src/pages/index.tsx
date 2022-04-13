30ba143c6677c197baae090259e64baa2b70b9b9
João Vitor <infamoz_br3@hotmail.com>
2022-04-13 15:01:34 -0300

    refactor: remove unused prop from SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ const Home = ({ product }: HomeProps) => {
             Get access to all  publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
